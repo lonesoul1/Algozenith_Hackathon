725f0cb7c7a6121d06414ac4f035e66a101b01c8
Divakar Kumar Gupta <131536312+lonesoul1@users.noreply.github.com>
2023-06-04 15:38:53 +0530

    completed scraping questions from leetcode

---
+++ lc.py
@@ -0,0 +1,48 @@
+# Import required packages
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from bs4 import BeautifulSoup
+
+# Define the chromedriver service
+s = Service('chromedriver.exe')
+
+# Instantiate the webdriver
+driver = webdriver.Chrome(service=s)
+
+# The base URL for the pages to scrape
+page_URL = "https://leetcode.com/problemset/all/?page="
+
+# Function to get all the 'a' tags from a given URL
+def get_a_tags(url):
+    driver.get(url)
+    time.sleep(7)
+    links=driver.find_elements(By.TAG_NAME,"a")
+    ans=[]
+    for i in links:
+        try:
+            if "/problems/" in i.get_attribute("href"):
+                ans.append(i.get_attribute("href"))
+        except:
+            pass
+    
+    ans=list(set(ans))
+    return ans
+
+my_ans=[]
+
+for i in range (1,55):
+    my_ans+=(get_a_tags(page_URL+str(i)))
+
+my_ans=(list(set(my_ans)))
+
+with open('lc.txt','a') as f:
+    for j in my_ans:
+        f.write(j+'\n')
+        
+print(len(my_ans))
+driver.quit()
